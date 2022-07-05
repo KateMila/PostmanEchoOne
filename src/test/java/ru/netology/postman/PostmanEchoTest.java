08d7f4529ba2db95eb3d99bfc558253cdc9b0f2c
KateMila <89808903+KateMila@users.noreply.github.com>
2022-07-05 23:26:46 +0300

    Initial commit

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.postman;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostToPostman() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some value"))
+        ;
+    }
+
+}
