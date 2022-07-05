8552aea1b1f64cb455230c30ad8d124ab31a4a96
KateMila <89808903+KateMila@users.noreply.github.com>
2022-07-05 23:35:14 +0300

    modified test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
+[![Build status](https://ci.appveyor.com/api/projects/status/7tdl88ic3pehv8xn?svg=true)](https://ci.appveyor.com/project/KateMila/postmanechoone)
 
--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -19,8 +19,37 @@ public class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 
+    @Test
+    void shouldReturnId() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("id:3")// отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("id:3"));
+    }
+
+    @Test
+    void shouldReturnCurrency() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency:RUB")// отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("currency:RUB"));
+    }
 }
